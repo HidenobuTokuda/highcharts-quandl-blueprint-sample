7a1a33fc72caafa2f70be60ff8627791823fbc6a
lethe1216 <lethe.16dec@gmail.com>
2019-01-05 16:56:33 -0400

    test

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import quandl
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    quandl.ApiConfig.api_key = os.environ.get('API_KEY') or "coMpzT82DE4TQXC8y_kG"
+    quandl.ApiConfig.api_key = os.environ.get('API_KEY') or ""
