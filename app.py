4a00a7f9f4c76d126f0089ac30ae18a540fd82ef
lethe1216 <lethe.16dec@gmail.com>
2019-01-06 14:14:29 -0400

    test

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-from app import app
+from flask import Flask
+from config import Config
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config.from_object(Config)
+bootstrap = Bootstrap(app)
+app.jinja_env.filters['zip'] = zip
+
+#from app import routes
+from app.routes import app1
+modules_define = [app1]
+for app1 in modules_define:
+    app.register_blueprint(app1)
+
+if __name__ == '__main__':
+    app.run()
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
-from app import app
-from flask import render_template
+from flask import render_template, Blueprint
 from app.model import getChartsData
 
-@app.route('/')
-@app.route('/index')
+app1 = Blueprint("app1", __name__, url_prefix="/app1", template_folder='templates')
+
+@app1.route('/')
+@app1.route('/index')
 def chart():
     chart_info1 = {"chart_title": 'Manufacturing PMI in US and China', "quandl_codes": ['ISM/MAN_PMI', 'NBSC/A190101_M'], "legends": ["ISM Manufacturing PMI","China NBSC Manufacturing PMI"], "units": ["pt", "pt"]}
     chart_info2 = {"chart_title": 'Non-Manufacturing PMI in US and China', "quandl_codes": ['ISM/NONMAN_NMI', 'NBSC/A190201_M'], "legends": ["ISM Non-Manufacturing NMI","China NBSC Non-Manufacturing NBI"], "units": ["pt", "pt"]}
