48968a430e537d038af8abedb7e849cf0322bc48
lethe1216 <lethe.16dec@gmail.com>
2019-01-06 10:45:10 -0400

    test

--- app/routes.py
+++ app/routes.py
@@ -10,4 +10,4 @@ def chart():
 
     charts, charts_comments = getChartsData(chart_info1,chart_info2)
 
-    return render_template('chart.html', title="Global Business Sentiment", charts=charts, charts_comments=charts_comments)
+    return render_template('app/chart.html', title="Global Business Sentiment", charts=charts, charts_comments=charts_comments)
